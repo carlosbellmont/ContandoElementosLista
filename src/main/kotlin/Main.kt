e168314e469e42d04c9c8adf0a5e9ff15f85778c
carlosbellmont <carlos.bellmont@gmail.com>
2021-07-28 14:54:30 +0200

    Initial Commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,24 @@
+import kotlin.random.Random
+
+fun main(){
+    val listaRandom = crearListaAleatorio()
+    val listaResultado = mutableListOf<Int>()
+
+    println(listaRandom)
+
+    for (numero in 0..9) {
+        var ocurrencias = 0
+        for (numRandom in listaRandom) {
+            if (numRandom == numero) {
+                ocurrencias++
+            }
+        }
+        listaResultado.add(numero, ocurrencias)
+    }
+
+    println(listaResultado)
+}
+
+fun crearListaAleatorio() : List<Int>{
+    return MutableList(Random.nextInt(90,110)){ Random.nextInt(0,9) }
+}
